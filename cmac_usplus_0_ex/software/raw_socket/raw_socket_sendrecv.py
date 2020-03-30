3d5430d479ca246e32d45eed4a6bb15ccf0ec1ed
Takefumi MIYOSHI <miyo@wasamon.net>
2020-03-30 11:20:55 +0000

    get mac address by using netifaces

--- cmac_usplus_0_ex/software/raw_socket/raw_socket_sendrecv.py
+++ cmac_usplus_0_ex/software/raw_socket/raw_socket_sendrecv.py
@@ -1,15 +1,18 @@
 import sys
 import socket
 import binascii
+import netifaces
 
 if __name__ == '__main__':
     ETH_P_ALL = 3
     dev = sys.argv[1]
     message = "Hello Alveo U50"
     data_bytes = int(sys.argv[2])//len(message) if len(sys.argv) > 2 else 1
-    
+
+    mac_addr_str = netifaces.ifaddresses(dev)[netifaces.AF_LINK][0]['addr']
     dest_addr  = b'\x00\x01\x02\x03\x04\x05' # destination address
-    src_addr   = b'\x98\x03\x9b\x1d\x63\x89' # source address
+    #src_addr   = b'\x98\x03\x9b\x1d\x63\x89' # source address
+    src_addr   = bytearray([int(x, 16) for x in mac_addr_str.split(':')])
     frame_type = b'\x34\x34' # frame type
     data = (message*data_bytes).encode() # payload
     
